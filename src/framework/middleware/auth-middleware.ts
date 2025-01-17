d7c5b730261f174ce89c979dce39bcb6c32564f0
4shaham <143824921+4shaham@users.noreply.github.com>
2025-01-17 17:11:36 +0530

    modify request interface

--- src/framework/middleware/auth-middleware.ts
+++ src/framework/middleware/auth-middleware.ts
@@ -1,27 +1,28 @@
 
-import { NextFunction, Request, Response } from "express";
+import { NextFunction,Response} from "express";
 import JwtService from "../utils/jwtService";
 import { StatusCode } from "../../enums/statusCode";
+import IRequest from "../../interface/others/IReqeust";
 
 const jwtService = new JwtService();
 
 const authorizationMiddleware = async (
-  req: Request,
+  req: IRequest,
   res: Response,
   next: NextFunction
 ) => {  
   try {
 
-    const authHeader = req.headers.authorization; // Access the Authorization header
-    console.log(authHeader)
+    const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return 
+       return
     }
-
     const token = authHeader.split(" ")[1]; 
     let verification = jwtService.verify(token);
-    console.log(verification)
+    req.userId=verification?.id 
+    req.email=verification?.email
     next();
+
   } catch (error) {
     res.status(401).json({ message:"userTokenExpired"});
   }
