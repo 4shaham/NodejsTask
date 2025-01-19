ee123978f75fb1e1e7c8ea9861b143cf60159854
4shaham <143824921+4shaham@users.noreply.github.com>
2025-01-19 09:31:14 +0530

    added 404 error hanlding

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,15 @@ const port=process.env.PORT
 // Swagger setup
 setupSwagger(app);
 
+// Catch-all route for 404 errors
+app.all('*', (req, res) => {
+    res.status(404).send({
+      error: 'Not Found',
+      message: `The requested URL ${req.originalUrl} was not found on this server.`,
+    });
+});
+
+
 
 
 app.listen(port,()=>console.log(`server running:http:localhost:${port}`))
